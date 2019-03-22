79f9b47cae24033a46d0ff82bea68e73cf52ddc7
Ramon Chavez <rachavez1974@gmail.com>
2019-03-22 00:08:32 -0400

    Finish prime number function

--- prime.rb
+++ prime.rb
@@ -1,1 +1,24 @@
 # Add  code here!
+def prime?(integer)
+  #an integer is a prime if and only if is not equal to 0, 1, 
+  #and can only be divisible by itself and 1
+  number = integer.to_i
+    
+    if number <= 1
+      return false
+    elsif number == 2 || number == 3
+        return true
+    else
+      #break integer in half
+      half = number/2
+        for divider in 2..half 
+          if number % divider == 0 
+            return false
+          end
+        end
+        return true
+    end
+end
+
+
+
